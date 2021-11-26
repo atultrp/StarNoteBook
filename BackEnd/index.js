f9fa4afff980045fbe9e75e0cf9a95ed65dbbf05
Atul Tripathi <atul31815@gmail.com>
2021-11-26 23:16:05 +0530

    Add files via upload

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,14 @@
 const connectToMongo = require("./database");
 const express = require('express')
+var cors = require('cors');
 
 // Connecting the backend to the datebase
 connectToMongo();
 
 const app = express();
 const port = 4000;
+
+app.use(cors());
 
 // It parses incoming requests with JSON payloads and is based on body-parser
 app.use(express.json());
