c8f5f7393b462bb30b923306cc6e3ca219de8a7b
Atul Tripathi <atul31815@gmail.com>
2021-12-13 00:04:02 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -32,8 +32,8 @@ app.use('/api/notes',require('./Routes/notes'));
 
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname,'..', 'build')));
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..','build', 'index.html'));
 });
 
